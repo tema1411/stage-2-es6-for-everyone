4f4d3048e6dd19f86ae958311e03e3e322e37ca5
Артём <33321841+tema1411@users.noreply.github.com>
2020-05-10 22:30:08 +0300

    add player control

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -2,25 +2,105 @@ import { controls } from '../../constants/controls';
 import { getRandomNumber } from '../helpers/mathHelper';
 
 export async function fight(firstFighter, secondFighter) {
-  console.log('firstFighter', firstFighter);
   return new Promise((resolve) => {
-    let { health: firstFighterHeals } = firstFighter;
-    let { health: secondFighterHeals } = secondFighter;
+    const PLAYER_ONE = 'player one';
+    const PLAYER_TWO = 'player two';
+    const pressedKeys = new Set();
 
-    window.addEventListener('keydown', function (e) {
-      switch (e.code) {
-        case controls.PlayerOneAttack:
-          //const hitPower = getHitPower(firstFighter);
-          const blockPower = getBlockPower(firstFighter);
+    let { health: firstFighterHealth } = firstFighter;
+    let { health: secondFighterHealth } = secondFighter;
+    let isCriticalHitOnePlayer = true;
+    let isCriticalHitTwoPlayer = true;
+
+    function controlCriticalHit(player) {
+      const intervalUseCriticalHit = 10000;
+      let timeoutCallback = null;
+
+      if (PLAYER_ONE) {
+        isCriticalHitOnePlayer = false;
+        timeoutCallback = () => (isCriticalHitOnePlayer = true);
+      } else if (PLAYER_TWO) {
+        isCriticalHitTwoPlayer = false;
+        timeoutCallback = () => (isCriticalHitTwoPlayer = true);
       }
+
+      setTimeout(timeoutCallback, intervalUseCriticalHit);
+    }
+
+    function isPressAllBtnFromArray(array) {
+      return array.every((btnCode) => pressedKeys.has(btnCode));
+    }
+
+    function getIsAttackingOnePlayer() {
+      return pressedKeys.has(controls.PlayerOneAttack);
+    }
+
+    function getIsAttackingTwoPlayer() {
+      return pressedKeys.has(controls.PlayerTwoAttack);
+    }
+
+    function getIsBlockingOnePlayer() {
+      return pressedKeys.has(controls.PlayerOneBlock);
+    }
+
+    function getIsBlockingTwoPlayer() {
+      return pressedKeys.has(controls.PlayerTwoBlock);
+    }
+
+    window.addEventListener('keydown', function ({ code }) {
+      console.log('pressedKeys', pressedKeys);
+      if (code === controls.PlayerOneAttack) {
+        if (!getIsAttackingOnePlayer() && !getIsBlockingTwoPlayer()) {
+          const damage = getDamage(firstFighter, secondFighter);
+          secondFighterHealth -= damage;
+          console.log('secondFighterHealth', secondFighterHealth);
+        }
+      }
+
+      if (code === controls.PlayerTwoAttack) {
+        if (!getIsAttackingTwoPlayer() && !getIsBlockingOnePlayer()) {
+          const damage = getDamage(secondFighter, firstFighter);
+          firstFighterHealth -= damage;
+          console.log('firstFighterHealth', firstFighterHealth);
+        }
+      }
+
+      if (isCriticalHitOnePlayer && isPressAllBtnFromArray(controls.PlayerOneCriticalHitCombination)) {
+        controlCriticalHit(PLAYER_ONE);
+
+        const damage = getCriticalDamage(firstFighter);
+        secondFighterHealth -= damage;
+        console.log('secondFighterHealth', secondFighterHealth);
+      }
+
+      if (isCriticalHitTwoPlayer && isPressAllBtnFromArray(controls.PlayerTwoCriticalHitCombination)) {
+        controlCriticalHit(PLAYER_TWO);
+
+        const damage = getCriticalDamage(secondFighter);
+        firstFighterHealth -= damage;
+        console.log('firstFighterHealth', firstFighterHealth);
+      }
+
+      pressedKeys.add(code);
     });
+
+    window.addEventListener('keyup', function ({ code }) {
+      pressedKeys.delete(code);
+    });
+
     // resolve the promise with the winner when fight is over
   });
 }
 
-function addControlFight() {}
+function getCriticalDamage(attacker) {
+  const gain = 2;
+  return gain * attacker.attack;
+}
 
-export function getDamage(attacker, defender) {}
+export function getDamage(attacker, defender) {
+  const damage = getHitPower(attacker) - getBlockPower(defender);
+  return damage < 0 ? 0 : damage;
+}
 
 export function getHitPower(fighter) {
   const criticalHitChance = getRandomNumber(1, 2);
